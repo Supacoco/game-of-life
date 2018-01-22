b02b75dfbbdbc8d2a532a2a4c060a3e2a9e547b7
Nicolas Corral <nico.corral@gmail.com>
2018-01-21 21:48:17 -0500

    feat: draw initial state grid

---
+++ index.js
@@ -0,0 +1,83 @@
+const RESOLUTION = 10
+const WIDTH = 400
+const HEIGHT = 400
+
+const ROWS = HEIGHT / RESOLUTION
+const COLS = WIDTH / RESOLUTION
+
+const setup = () => {
+  const canvas = document.createElement('canvas')
+  canvas.id = 'canvas'
+  canvas.width = WIDTH
+  canvas.height = HEIGHT
+
+  document.getElementById('root').appendChild(canvas)
+  const grid = generate2DGrid(COLS, ROWS)
+  for (let i = 0; i < COLS; i++) {
+    for (let j = 0; j < ROWS; j++) {
+      grid[i][j] = Math.round(Math.random(2));
+    }
+  }
+  return grid
+}
+
+const generate2DGrid = (cols, rows) => {
+  let grid = new Array(cols)
+  for (let i = 0; i < grid.length; i++) {
+    grid[i] = new Array(rows)
+  }
+  return grid
+}
+
+const draw = (grid) => {
+  const context = document.getElementById('canvas').getContext('2d')
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLS; j++) {
+      if (grid[i][j]){
+        context.fillStyle = 'white';
+      } else {
+        context.fillStyle = 'black';
+      }
+      context.fillRect(j * RESOLUTION, i * RESOLUTION, RESOLUTION, RESOLUTION)
+    }
+  }
+}
+
+const generateNewState = (grid) => {
+  const newGrid = generate2DGrid(COLS, ROWS)
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLS; j++) {
+      const neighboors = countNeighbors(grid, i, j)
+      const currentCell = grid[i][j]
+      if (currentCell === 0 && neighboors === 3) {
+        newGrid[i][j] = 1
+      } else if (currentCell === 1 && (neighboors < 2 || neighboors > 3)) {
+        newGrid[i][j] = 0
+      } else {
+        newGrid[i][j] = currentCell
+      }
+    }
+  }
+  return newGrid
+}
+
+const countNeighbors = (grid, x, y) => {
+  let sum = 0
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + COLS) % COLS
+      let row = (y + j + ROWS) % ROWS
+      sum += grid[col][row]
+    }
+  }
+  sum -= grid[x][y]
+  return sum
+}
+
+let state = setup()
+draw(state)
+
+setInterval(() => {
+  state = generateNewState(state)
+  draw(state)
+}, 16)
